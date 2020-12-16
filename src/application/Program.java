f1509c62d6d7c69eb7da087ca3eeef7304fbdd81
motasjw <lucasm.lopes087@gmail.com>
2020-12-16 10:08:30 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Program {
 		
 		Seller obj2 = new Seller(21, "João Lima", "joao@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(obj2);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
